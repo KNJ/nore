9a21d3236e956653e009d9c94ca5086fb6a2c3cd
KNJ <knj@wazly.net>
2015-04-25 23:39:07 +0900

    Distribute the output file into expanded version and compressed version

--- config.rb
+++ config.rb
@@ -1,3 +1,12 @@
 css_dir = "dist/css"
 sass_dir = "src/scss"
-output_style = :compressed
+output_style = (environment == :production) ? :compressed : :expanded
+
+if environment == :production
+  on_stylesheet_saved do |file|
+    if File.exists?(file)
+      filename = File.basename(file, File.extname(file))
+      File.rename(file, css_dir + "/" + filename + ".min" + File.extname(file))
+    end
+  end
+end
